7e86762afe4c5ae8a52a7cced65926b2c9d494c8
Jacob <jacobemitchell14@gmail.com>
2024-02-14 23:41:51 -0700

    spent hour making dropdown

--- Code/taskedChecklist.js
+++ Code/taskedChecklist.js
@@ -1,43 +1,73 @@
 document.getElementById("newTaskBtn").addEventListener("click", function() {
-    var taskList = document.getElementById("taskList");
+    let taskList = document.getElementById("taskList");
     
-    var taskItem = document.createElement("div");
+    let taskItem = document.createElement("div");
     taskItem.classList.add("task-item");
     
-   
-    var checkbox = document.createElement("input");
+    let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.name = "task";
     
-    var label = document.createElement("label");
-    
-    var input = document.createElement("input");
+    let input = document.createElement("input");
     input.type = "text";
     input.maxLength = 20; 
     
+
+    let rating = createDropdown();
+    
     taskItem.appendChild(checkbox);
     taskItem.appendChild(input);
-    taskItem.appendChild(label);
+    taskItem.appendChild(rating);
     
     taskList.appendChild(taskItem);
     
     input.focus();
     
-    input.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            label.textContent = input.value;
-            try {
-                taskItem.removeChild(input);
-            } catch (error) {
-            }
-        }
+    input.addEventListener("input", function() {
+        createLabelIfReady();
     });
+    
 
-    input.addEventListener("blur", function() {
-        label.textContent = input.value;
-        try {
-            taskItem.removeChild(input);
-        } catch (error) {
-        }
+    rating.addEventListener("change", function() {
+        createLabelIfReady();
     });
+    
+    function createLabelIfReady() {
+        let inputValue = input.value.trim();
+        let selectValue = rating.value;
+        
+        if (inputValue && selectValue !== "none") {
+            var label = document.createElement("label");
+            label.textContent = inputValue + " - " + selectValue;
+            taskItem.appendChild(label);
+            
+            taskItem.removeChild(input);
+            taskItem.removeChild(rating);
+        }
+    }
 });
+
+function createDropdown() {
+    let rating = document.createElement("select");
+    let none = document.createElement("option");
+    none.value = "none";
+    none.textContent = "Select Difficulty";
+    rating.appendChild(none);
+    
+    let easy = document.createElement("option");
+    easy.value = "Easy";
+    easy.textContent = "Easy";
+    rating.appendChild(easy);
+    
+    let medium = document.createElement("option");
+    medium.value = "Medium";
+    medium.textContent = "Medium";
+    rating.appendChild(medium);
+    
+    let hard = document.createElement("option");
+    hard.value = "Hard";
+    hard.textContent = "Hard";
+    rating.appendChild(hard);
+    
+    return rating;
+}
