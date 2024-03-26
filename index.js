7a47b176062fda5bb8569205af2b1696062ed8aa
Jacob <jacobemitchell14@gmail.com>
2024-03-25 22:06:41 -0600

    Fix privacy setting for quote call

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require('path');
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/TaskShare");
+const fetch = require('node-fetch');
 
 dotenv.config();
 
@@ -32,6 +33,26 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
 app.use(express.static('./'));
+
+app.get('/api/quotes', async (req, res) => {
+  try {
+      const response = await fetch('https://type.fit/api/quotes');
+      const data = await response.json();
+      res.json(data);
+  } catch (error) {
+      console.error('Error fetching quotes:', error);
+      res.status(500).json({ error: 'Failed to fetch quotes' });
+  }
+});
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; connect-src 'self' https://type.fit; style-src 'self' 'unsafe-inline'"
+  );
+  next();
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
